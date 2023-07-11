a8f401722e5b9a07a89c6d1aedf8aa36a01ca2d3
Justin <Justin@grandcircus.co>
2023-07-11 10:01:41 -0400

    room calc

---
+++ Unit 2 Fundementals/RoomCalc/RoomCalc/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Welcome to the room calculator");
+//getting user input
+Console.WriteLine("Please enter a length.");
+float length = float.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a width.");
+float width = float.Parse(Console.ReadLine());
+Console.WriteLine("Please enter a height.");
+float height = float.Parse(Console.ReadLine());
+//calculations
+float area = width * length;
+float perimeter = 2 * (length + width);
+float volume = length * width * height;
+float surfaceArea = 2 * ((width * length)+(height*length)+(height*width));
+Console.WriteLine($"Area: {area}");
+Console.WriteLine($"Perimeter: {perimeter}");
+Console.WriteLine($"Volume: {volume}");
+Console.WriteLine($"Surface Area: {surfaceArea}");
+//room size
+if(area <= 250)
+{
+    Console.WriteLine("This room is small.");
+}
+else if (area > 250 && area < 650)
+{
+    Console.WriteLine("This room is medium");
+}
+else
+{
+    Console.WriteLine("This room is large");
+}
